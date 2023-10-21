f33031219e4b81fad7d69c6f092c505b458cf502
switt_tv <4galarzajuan2004@gmail.com>
2023-10-21 02:54:35 -0600

    agregando el jugador

--- python/main.py
+++ python/main.py
@@ -7,12 +7,17 @@ SCREEN_HEIGHT = 1080
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-run = True
+player = pygame.Rect((100, 150, 50, 50))
 
+run = True
 while run:
+
+    pygame.draw.rect(screen, (250, 0, 0), player)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
