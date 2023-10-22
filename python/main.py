305ffdea0289199b3fd7ba3b8c4d048c4d4657a7
switt_tv <4galarzajuan2004@gmail.com>
2023-10-22 00:00:41 -0600

    haciendo la clase del jugador y agregandole movimiento

--- python/main.py
+++ python/main.py
@@ -1,23 +1,78 @@
 import pygame
+import sys
 
 pygame.init()
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player_speed = 4
 
-player = pygame.Rect((100, 150, 50, 50))
+screenGame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 
-run = True
-while run:
 
-    pygame.draw.rect(screen, (250, 0, 0), player)
 
+class Player:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 32, 32)
+        
+        self.x = int(x)
+        self.y = int(y)
+        
+        self.velX = 0
+        self.velY = 0
+        
+        self.color = ("aqua")
+
+        self.left_pressed = False
+        self.right_pressed = False
+
+        self.speed = 4
+    
+    def draw(self, screenGame):
+        pygame.draw.rect(screenGame, self.color, self.rect)
+    
+    def update(self):
+
+        self.velX = 0
+        self.velY = 0
+
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+            
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+
+        self.x += self.velX
+        self.y += self.velY
+
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+
+player = Player(SCREEN_HEIGHT/2, SCREEN_WIDTH/2)
+
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            pygame.quit()
+            sys.exit()
+        
+    keys = pygame.key.get_pressed()
 
-    pygame.display.update()
+    if keys[pygame.K_a]:
+        player.left_pressed = True
+        player.right_pressed = False  # Asegúrate de desactivar la tecla opuesta
+    elif keys[pygame.K_d]:
+        player.right_pressed = True
+        player.left_pressed = False  # Asegúrate de desactivar la tecla opuesta
+    else:
+        player.left_pressed = False
+        player.right_pressed = False
+        
+    screenGame.fill((12, 24, 36))
+    player.draw(screenGame)
 
-pygame.quit()
+    player.update()
+    pygame.display.flip()
+    clock.tick(60)
